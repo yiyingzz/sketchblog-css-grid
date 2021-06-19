b9394cd0c859c8283419b8bea0d065eb7288976b
Yiying Zou <yiyingzou@gmail.com>
2021-06-19 17:02:05 -0400

    more tweaking to get it to work on chrome

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # sketchblog-css-grid
+
 css grid-based masonry-style sketch blog
 
 Visit the live site here: https://yiying.ca/sketch/
 
-- masonry layout tutorial from here: https://medium.com/@andybarefoot/a-masonry-style-layout-using-css-grid-8c663d355ebb 
-- live tutorial example here: https://codepen.io/andybarefoot/pen/QMeZda
+- masonry layout tutorial from here: https://medium.com/@andybarefoot/a-masonry-style-layout-using-css-grid-8c663d355ebb
+- a follow up to the original tutorial that fixes some issues: https://medium.com/@RRafatpanah/first-of-all-thanks-so-much-for-this-great-article-it-was-exactly-what-i-was-hoping-to-find-b2ad245b127e
 
+## notes
 
-## known issues
+- so I used both the tutorials above, the first one to create the layout, then made the changes specified in the second one to fix some minor things
+- however, I discovered a few remaining issues and made some changes of my own:
 
-- seems to breakdown on mobile (at least on my phone which is admittedly pretty old) 
-- I used smaller row heights than the tutorial (2px) which creates a ton of rows so that might cause a problem when there's too many
-- images might be loading in at different times (not sure if this would make a difference?)
-- my images are pretty big --> maybe try `srcset`
+- first, I put `align-items: self-start;` into the CSS instead of re-adding it everytime through JS
+- it seems like the default is more or less the same thing visually
+
+- second, Chrome (and Edge, which also uses Chromium) only allows 1001 rows in a grid so this layout will quickly break on mobile (and also on desktop/tablet if there's a lot of content) if you set your `grid-auto-rows` to a very small value, which I did in order to make the spacing more uniform
+- you can actually see this happening on the codepen example in the 2nd tutorial since the dev made his rows 1px in height
+- this isn't a huge problem but it does mean that the spacing between the items aren't exact (the first tutorial talks about this, basically the greater the `grid-auto-rows` value is, the more varied the extra space is at the end of the content for each item)
--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-function resizeGridItem(item) {
-  const grid = document.querySelector('.grid');
-  const rowHeight = parseInt(
-    window.getComputedStyle(grid).getPropertyValue('grid-auto-rows')
-  );
-  const rowGap = parseInt(
-    window.getComputedStyle(grid).getPropertyValue('row-gap')
-  );
-  const rowSpan = Math.ceil(
-    (item.querySelector('.content').getBoundingClientRect().height + rowGap) /
-      (rowHeight + rowGap) +
-      30
-  );
-  item.style.gridRowEnd = 'span ' + rowSpan;
+const grid = document.querySelector('.grid');
+const allItems = grid.querySelectorAll('.item');
+
+function resize() {
+  const rowHeight = getStyleValue(grid, 'grid-auto-rows');
+  const rowGap = getStyleValue(grid, 'row-gap');
+  grid.style.gridAutoRows = 'auto';
+  allItems.forEach((item) => {
+    item.style.gridRowEnd = `span ${Math.ceil(
+      (item.clientHeight + rowGap) / (rowHeight + rowGap)
+    )}`;
+  });
+  grid.removeAttribute('style');
+}
+// we save the vales for row height & row gap
+// then set grid-auto-rows to auto --> which makes it automatically adjust to the content size
+// set align-items to self-start --> like 'start', it aligns to start of cell but also along the item's starting side
+// then set grid-row-end to (height of item + row gap) divided by (row height + row gap)
+// since row gap is set to 0, this is basically height of item divided by row height
+// which gives us the number of rows it spans over
+// then we remove the attribute "style" which resets grid-auto-rows to default (b/c we remove the inline css)
+
+function getStyleValue(element, property) {
+  return parseInt(window.getComputedStyle(element).getPropertyValue(property));
 }
 
-function resizeAllGridItems() {
-  const allItems = document.querySelectorAll('.item');
-  for (let i = 0; i < allItems.length; i++) {
-    imagesLoaded(allItems[i], resizeInstance);
-  }
-}
-
-function resizeInstance(instance) {
-  const item = instance.elements[0];
-  resizeGridItem(item);
-}
-
-window.onload = resizeAllGridItems();
-window.addEventListener('resize', resizeAllGridItems);
+window.addEventListener('load', resize);
+window.addEventListener('resize', resize);
 
 console.log(`
 ⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣠⣤⣶⣶
