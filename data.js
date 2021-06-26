f546b4daae04236c35b2a42e8ed46d8bf0aa557a
yiyingzz <yiyingzou@gmail.com>
2021-06-25 20:34:11 -0400

    using data from js

---
+++ data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    file: "2021-06-05.jpg",
+    text: "amphitheatre at Guild Park, plein air painting. was nice to go outside & paint!",
+    imgDesc: "watercolour painting"
+  },
+  {
+    file: "2021-03-12.jpg",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a hendrerit augue, id accumsan lectus. Nunc condimentum mauris non dui maximus tincidunt et in augue. Nunc egestas volutpat tortor, vitae luctus diam iaculis id. Nulla facilisi.\n\n<br> Ut sapien lectus, egestas tincidunt ligula ut, scelerisque viverra metus. Pellentesque placerat dignissim metus. Donec ligula orci, aliquam ut lorem quis, efficitur ultrices dolor. Integer ut semper nunc. Curabitur placerat,\n\n lorem fringilla euismod sodales, augue nulla consectetur odio, nec posuere lectus enim eu dui. Pellentesque et tincidunt augue. Quisque vehicula eget turpis a cursus. ",
+    imgDesc: "painting"
+  },
+  {
+    file: "2021-03-04.jpg",
+    text: "amphitheatre at Guild Park,\n plein air painting.\n was nice to go outside & paint!"
+  },
+  {
+    file: "2021-02-04.jpg",
+    text: ""
+  },
+  {
+    file: "2021-01-29.jpg",
+    text: "text"
+  },
+  {
+    file: "2021-01-16.jpg",
+    text: "virtual plein air"
+  }
+];
